4383e6e70419df5530bee93f6c3abbd2e2890d65
Stephan Schaffner <63955136+Hornochs@users.noreply.github.com>
2021-10-16 17:13:29 +0200

    Missed one copy line to make it work in CheckMK
    There was a path missing that CheckMK gets the new check.

--- copy-to-target.sh
+++ copy-to-target.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cp lmsensors2.py ~/local/lib/check_mk/base/plugins/agent_based/
+cp lmsensors2.py ~/local/share/check_mk/checks/lmsensors2
 cp package-info.py ~/var/check_mk/packages/lmsensors2
 cp agent/lmsensors2 ~/local/share/check_mk/agents/plugins/
